bedc1462e40ed6b28d3a189fee280fe5f11eff86
rickdouglas <rickmiranda23@gmail.com>
2021-06-15 15:36:48 -0300

    Desafios sobre conceitos de React JS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from "react";
+import {useState, useEffect} from 'react';
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  
+  const [repositories, setRepositories] = useState([]) ;
+
+  useEffect(() =>{
+    api.get('repositories').then(response => {
+        setRepositories(response.data);
+    })
+}, []); // passa-se um array vazio no fim do Hook caso queira que seja executado somente
+
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: ' Adding a new repository test ',
+      url: 'Rick',
+      techs:['java', 'react', 'node']
+  });
+
+  const repository = response.data; 
+  setRepositories([...repositories, repository]);
+
   }
 
+
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`); // chamada a api passando o id do elemento a ser excluido
+
+    setRepositories(repositories.filter(repository =>  // atualiza o vetor de elementos mantendo no array apenas os elemento com o id diferente do que foi passado na url
+      repository.id !== id));
+
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+      {repositories.map(repository => (
+        <li key={repository.id}>
+          {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
         </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
